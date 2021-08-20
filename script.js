7fdec1922c58f2ddb6f121e1a8941ca53996c06e
aromalsanthosh <aromalponmana@gmail.com>
2021-08-20 22:50:26 +0530

    Added Edit Button Functionality, Added comments👾

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-const input = document.querySelector("#add");
-const  btn = document.querySelector("#bttn");
-const list = document.querySelector("#item");
-var element = document.getElementsByTagName('li');
+const input = document.querySelector("#add"); //text box
+const  btn = document.querySelector("#bttn"); // Add button
+const list = document.querySelector("#item"); // li item
+var element = document.getElementsByTagName('li'); 
 
+//function to edit todo
+function editTodo(){
+  const edit = document.getElementsByClassName('edit');
+  for (var index = 0; index <edit.length; index++){
+    edit[index].addEventListener("click",
+      function(){
+      var text = prompt("Edit This To-Do ");
+      this.closest(".task").innerHTML = `<button id="edt" class="edit"><i class="fas fa-pencil-alt"></i></button>${text}<button id="dlt"    class="delete">X</button>`;
+    }
+    )
+ }
+}
+editTodo();
 
+//function to add todo
 btn.onclick = function(){
-    
+
     var txt = input.value;
     if(txt ==''){
         alert('Add some text');
@@ -19,13 +33,14 @@ btn.onclick = function(){
     }
     
 };
-
+//function to mark todo as done (strikethrough)
 list.onclick = function(ev){
     if(ev.target.tagName == 'LI'){
          ev.target.classList.toggle('checked');
     }
 }
 
+// function to delete todo
 function addDeleteEvents(){
   var tasks = document.querySelectorAll(".task");
    for (var index = 0; index <tasks.length; index++){
@@ -36,16 +51,14 @@ function addDeleteEvents(){
       function(){
          this.closest(".task").remove();
       });
-    //   tasks[index].querySelector("#edt").addEventListener("click",
-    //   function(){
-        
-    //   });
+    
 
    }
 }
 
 addDeleteEvents();
 
+//function to add odo on enter key press
 document.getElementById("add")
     .addEventListener("keyup", function(event) {
     event.preventDefault();
